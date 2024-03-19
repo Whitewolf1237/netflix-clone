0235664dcdb94725301450f3cfb606f8d10fa723
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-03-19 20:51:27 +0530

    validator

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import {checkValidData} from '../utils/validate.js'
 
 
 const Login = () => {
+
+    //HOOKS
     const [isSignInForm,setIsSignInForm]=useState(true);
+    const[errorMessage,setErrorMessage]=useState(null)
+    const email=useRef(null)
+    const password=useRef(null)
+    const name=useRef(null)
+
     const toggleSignInForm=()=>{
         setIsSignInForm(!isSignInForm)
     }
 
 const handleButtonClick = () =>{
     //Validate form data
+    const message = checkValidData(email.current.value,password.current.value)
+    setErrorMessage(message);
 }
 
 return (
@@ -19,21 +29,23 @@ return (
         <div className='absolute'>
             <img src="https://i.pinimg.com/originals/19/8b/2f/198b2f01e73b905772279616eccc7c65.jpg" alt='bg-pic' />
         </div>
-        <form className='bg-[#000000b3] bg-opacity-80 rounded-lg p-12 absolute w-3/12 mt-36 mx-auto right-0 left-0 text-white'>
+        <form onSubmit={(e)=>e.preventDefault()} className='bg-[#000000b3] bg-opacity-80 rounded-lg p-12 absolute w-3/12 mt-36 mx-auto right-0 left-0 text-white'>
             <h1 
                 className='font-bold text-3xl py-4'>
                     {isSignInForm ? "Sign In" : "Sign Up"}
             </h1>
 
-            {!isSignInForm && (<input type="text" placeholder='Full Name' 
+            {!isSignInForm && (<input ref={name} type="text" placeholder='Full Name' 
                 className='p-4 my-4 w-full bg-[#000000b3] border border-solid border-gray-500 bg-opacity-80 rounded-md' />)}
 
-            <input type="text" placeholder='Email or phone number' 
+            <input ref={email} type="text" placeholder='Email' 
                 className='p-4 my-4 w-full bg-[#000000b3] border border-solid border-gray-500 bg-opacity-80 rounded-md' />
             
-            <input type="password" placeholder='Password' 
+            <input ref={password} type="password" placeholder='Password' 
                 className='p-4 my-4 w-full bg-[#000000b3] border border-solid border-gray-500 bg-opacity-80 rounded-md' />
             
+            <p className='text-[#ff3333] text-lg font-bold'>{errorMessage}</p>
+
             <button 
                 className='cursor-pointer p-3 my-2 bg-red-700 w-full rounded-md' onClick={handleButtonClick}>
                 {isSignInForm ? "Sign In" : "Sign Up"}
